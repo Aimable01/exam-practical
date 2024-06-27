1cec92a034c4999245be700b5c61b042225ec25b
Aimable01 <aimable.kwizera14@gmail.com>
2024-06-27 15:19:17 +0200

    routes

---
+++ backend/routes/routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  addTask,
+  getAllTasks,
+  getSingleTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskControllers");
+
+const Router = express.Router();
+
+Router.post("/tasks", addTask);
+Router.get("/task", getAllTasks);
+Router.get("/tasks/:id", getSingleTask);
+Router.put("/tasks/:id", updateTask);
+Router.delete("/tasks/:id", deleteTask);
+
+module.exports = Router;
